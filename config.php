6109e898d9614e8dde75824fbe5c5584a8993bc3
pauljere97 <66921860+pauljere97@users.noreply.github.com>
2021-05-25 09:19:25 +0200

    lesson single

--- config.php
+++ config.php
@@ -6,12 +6,12 @@ $sql_db_host = "localhost";
 // MySQL Database User
 $sql_db_user = "root";
 // MySQL Database Password
-$sql_db_pass = "root";
+$sql_db_pass = "";
 // MySQL Database Name
 $sql_db_name = "kaderi";
 
 // Site URL
-$site_url = "http://localhost:8888/kaderidegistiren/";
+$site_url = "http://localhost/pauljere/kaderi-full/";
 
 
 
