b6fcbe04edc578ddfa83670f73d7abf0ca99edf2
pauljere97 <66921860+pauljere97@users.noreply.github.com>
2021-05-31 20:09:35 +0200

    nav active

--- config.php
+++ config.php
@@ -7,12 +7,12 @@ $sql_db_host = "localhost";
 $sql_db_user = "root";
 // MySQL Database Password
 //$sql_db_pass = "";
-$sql_db_pass = "root";
+$sql_db_pass = "";
 // MySQL Database Name
 $sql_db_name = "kaderi";
 
 // Site URL
-$site_url = "http://localhost:8888/kaderi-full/";
+$site_url = "http://localhost/pauljere/kaderi-full/";
 
 
 
